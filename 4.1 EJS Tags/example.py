a315d4ec6027e00c48c9ce3e88f3da5c96f8d0cb
shivamdevloper123 <shivam71066@gmail.com>
2024-08-06 01:42:08 +0530

    Normalize line endings to CRLF

---
+++ 4.1 EJS Tags/example.py
@@ -0,0 +1,12 @@
+# define GFG() function using def keyword
+def GFG():
+	i=10
+	# check i is odd or not 
+	# using if and else keyword
+	if(i % 2 == 0):
+		return "given number is even";
+	else:
+		return("given number is odd") 
+	
+# call GFG() function 
+GFG()
